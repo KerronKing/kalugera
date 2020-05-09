5eec6502ced59c8bc951b62dbc5dfb5aefc82455
KerronKing <king_of_palmiste@hotmail.com>
2020-05-09 12:47:51 -0400

    chore: add actions to user, article and vote controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
   end
 
   def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,21 @@
 class VotesController < ApplicationController
   def new
+    @vote = current_user.votes.build
   end
 
   def create
+    @vote = current_user.votes.build(vote_params)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @vote.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def vote_params
+    params.require(:vote).permit(:user_id, :article_id)
   end
 end
